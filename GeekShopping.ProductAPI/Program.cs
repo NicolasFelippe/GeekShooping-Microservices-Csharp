f3ecbfca6448c1714f6e56b105be99376b38047e
Nicolas Felippe Rocha <55363641+NicolasFelippe@users.noreply.github.com>
2022-02-06 14:50:50 -0300

    Product Api

--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -1,3 +1,9 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
+using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Config Context MariaDb
+var connection = builder.Configuration.GetConnectionString("MariaDbConnection");
+builder.Services.AddDbContext<MariaDbContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(10, 6, 5))));
+
+
+// Config AutoMapper
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Repositories
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
---
+++ GeekShopping.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Model;
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Repository {
+    public class ProductRepository : IProductRepository {
+
+        private readonly MariaDbContext _context;
+        private readonly IMapper _mapper;
+        public ProductRepository(MariaDbContext context, IMapper mapper) {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ProductVO>> FindAll() {
+            return _mapper.Map<List<ProductVO>>(await _context.Products.ToListAsync());
+        }
+
+        public async Task<ProductVO> FindById(long id) {
+            return _mapper.Map<ProductVO>(await _context.Products.FirstOrDefaultAsync(product => product.Id == id));
+        }
+
+        public async Task<ProductVO> Create(ProductVO productVO) {
+            Product product = _mapper.Map<Product>(productVO);
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<ProductVO>(product);
+        }
+
+        public async Task<ProductVO> Update(ProductVO productVO) {
+            Product product = _mapper.Map<Product>(productVO);
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<ProductVO>(product);
+        }
+
+        public async Task<bool> Delete(long id) {
+            try {
+                Product product = _mapper.Map<Product>(await FindById(id));
+                if (product == null) return false;
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+                return true;
+            } catch (Exception) {
+
+                return false;
+            }
+        }
+
+
+    }
+}
